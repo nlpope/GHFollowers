c423d5bf62c4c97433837e2aec5d4751b83e629a
Noah Pope <npope@tutanota.com>
2024-05-25 03:08:12 -0500

    adding logo, textfield & button to SearchVC

---
+++ GHFollowers/Views/GFButton.swift
@@ -0,0 +1,38 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Noah Pope on 5/24/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    //initializer for storyboards (which we arent using)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius      = 10
+        titleLabel?.textColor   = .white
+        titleLabel?.font        = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
