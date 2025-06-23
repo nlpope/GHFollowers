e82ce6bed660a6ce57bfc984bae6dd3e35eb3aad
Noah Pope <npope@tutanota.com>
2025-06-23 10:30:46 -0500

    fixing keyboard handler

--- GHFollowers/Utilities/KeyboardHandler+Utils.swift
+++ GHFollowers/Utilities/KeyboardHandler+Utils.swift
@@ -40,73 +40,7 @@ extension UIViewController
             view.frame.origin.y = newFrameY
         }
     }
-    /**
-     * DELETE THE BELOW
-     * Solve for keyboard blocking textfield (by SwiftArcade)
-     > YT source: https://www.youtube.com/watch?v=O4tP7egAV1I&ab_channel=SwiftArcade
-     > Git source: https://github.com/jrasmusson/ios-professional-course/blob/main/Password-Reset/7-Dealing-Keyboards/README.md
-     */
     
-    import UIKit
-    
-    //-------------------------------------//
-    // MARK: - SOLVE FOR KEYBOARD BLOCKING TEXTFIELD (by SwiftArcade)
-    
-    extension UIViewController
-    {
-        func setupKeyboardHiding() {
-            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-            
-            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-        }
-        
-        
-        @objc func keyboardWillShow(sender: NSNotification) {
-            // get keyboard measurements & currently focused text field location
-            guard let userInfo = sender.userInfo,
-                  let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
-                  let currentTextField = UIResponder.currentResponder() as? UITextField else { return }
-            
-            // check if keyboard top is above text field bottom (relative to superview)
-            let keyboardTopY        = keyboardFrame.cgRectValue.origin.y
-            let textFieldBottomY    = currentTextField.frame.origin.y + currentTextField.frame.size.height
-            
-            // bump view up if so: moving up/left = negative val, moving down/right = positive val
-            if textFieldBottomY > keyboardTopY {
-                let textFieldTopY   = currentTextField.frame.origin.y
-                let newFrameY       = (textFieldTopY - keyboardTopY / 2) * -1
-                view.frame.origin.y = newFrameY
-            }
-        }
-        
-        
-        @objc func keyboardWillHide(sender: NSNotification) { view.frame.origin.y = 0 }
-    }
-    
-    
-    extension UIResponder
-    {
-        private struct Static
-        {
-            static weak var responder: UIResponder?
-        }
-        
-        
-        // determines which element onscreen triggered the keyboard
-        static func currentResponder() -> UIResponder?
-        {
-            Static.responder = nil
-            UIApplication.shared.sendAction(#selector(UIResponder._trap),
-                                            to: nil,
-                                            from: nil,
-                                            for: nil)
-            return Static.responder
-        }
-        
-        
-        @objc private func _trap() { Static.responder = self }
-    }
-
     
     @objc func keyboardWillHide(sender: NSNotification) { view.frame.origin.y = 0 }
 }
